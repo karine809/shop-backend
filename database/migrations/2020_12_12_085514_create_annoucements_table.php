f59419dd86f0c77df4551d4fd67b898d751b2f4c
karine809 <karinemesropyan1984@gmail.com>
2020-12-18 16:19:33 +0400

    initial commit

---
+++ database/migrations/2020_12_12_085514_create_annoucements_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnoucementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('annoucements', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->string('deadline')->nullable();
+            $table->string('solary')->nullable();
+            $table->timestamps();
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('annoucements');
+    }
+}
